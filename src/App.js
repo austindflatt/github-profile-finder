97b8829902c0ae3674ce0115011ae7b594ffe6a3
Austin Flatt <80080997+austindflatt@users.noreply.github.com>
2022-03-02 01:31:06 -0500

    created NavBar component, and added Material UI container component in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import NavBar from './components/Layout/NavBar';
 
 function App() {
   return (
     <Router>
-      <h1>Hello World</h1>
+      <NavBar />
+      <CssBaseline />
+      <Container maxWidth="xl">
+        <h1>Hello World</h1>
+      </Container>
     </Router>
   );
 }
--- src/components/Layout/NavBar.jsx
+++ src/components/Layout/NavBar.jsx
@@ -1,9 +1,116 @@
-import React from 'react'
+import React, { useState } from 'react';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-function NavBar() {
+const pages = ['Home', 'About'];
+
+function NavBar({title}) {
+
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+  setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+  setAnchorElNav(null);
+  };
+
   return (
-    <div>NavBar</div>
+    <AppBar position="static">
+        <Container maxWidth="xl">
+            <Toolbar disableGutters>
+                <Typography
+                variant="h6"
+                noWrap
+                component="div"
+                sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
+                >
+                    GitHub Profile Finder
+                    </Typography>
+                <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                    <IconButton
+                    size="large"
+                    aria-label="account of current user"
+                    aria-controls="menu-appbar"
+                    aria-haspopup="true"
+                    onClick={handleOpenNavMenu}
+                    color="inherit"
+                    >
+                    <MenuIcon />
+                    </IconButton>
+                    <Menu
+                    id="menu-appbar"
+                    anchorEl={anchorElNav}
+                    anchorOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'left',
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'left',
+                    }}
+                    open={Boolean(anchorElNav)}
+                    onClose={handleCloseNavMenu}
+                    sx={{
+                        display: { xs: 'block', md: 'none' },
+                    }}
+                    >
+                        <MenuItem key={null} onClick={handleCloseNavMenu}>
+                            <Typography
+                            component={Link}
+                            to={'/'}
+                            textAlign="center"
+                            >
+                                About
+                            </Typography>
+                        </MenuItem>
+                    </Menu>
+                </Box>
+                <Typography
+                variant="h6"
+                noWrap
+                component="div"
+                sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
+                >
+                    <GitHubIcon />
+                </Typography>
+                <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                    <Button
+                    component={Link}
+                    to={'/'}
+                    key={null}
+                    onClick={null}
+                    sx={{ my: 2, color: 'white', display: 'block' }}
+                    >
+                        Home
+                    </Button>
+                </Box>
+            </Toolbar>
+        </Container>
+    </AppBar>
   )
+}
+
+NavBar.defaultProps = {
+    title: 'GitHub Profile Finder',
+}
+
+NavBar.propTypes = {
+    title: PropTypes.string,
 }
 
 export default NavBar
