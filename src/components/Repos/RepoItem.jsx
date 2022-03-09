53f270aaa5bbb8c6da35f0db83429d1f7e3d4dbc
Austin Flatt <80080997+austindflatt@users.noreply.github.com>
2022-03-09 00:47:55 -0500

    Updated RepoItem, moved all data

--- src/components/Repos/RepoItem.jsx
+++ src/components/Repos/RepoItem.jsx
@@ -1,9 +1,55 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import ListItem from '@mui/material/ListItem'
+import ListItemText from '@mui/material/ListItemText'
+import Chip from '@mui/material/Chip'
+import Stack from '@mui/material/Stack'
+import ListItemAvatar from '@mui/material/ListItemAvatar'
+import InsertLinkIcon from '@mui/icons-material/InsertLink'
+import VisibilityIcon from '@mui/icons-material/Visibility'
+import StarIcon from '@mui/icons-material/Star'
+import InfoIcon from '@mui/icons-material/Info'
+import RestaurantIcon from '@mui/icons-material/Restaurant'
+import Divider from '@mui/material/Divider'
+import Typography from '@mui/material/Typography'
 
-const RepoItem = () => {
+const RepoItem = ({ repo }) => {
   return (
-	<div>RepoItem</div>
+	<>
+  <ListItem alignItems="flex-start">
+      <ListItemAvatar>
+        <InsertLinkIcon />
+      </ListItemAvatar>
+      <ListItemText
+      primary={repo.name}
+      secondary={
+        <React.Fragment>
+          <Typography
+            sx={{ display: 'inline' }}
+            component="span"
+            variant="body2"
+            color="text.primary"
+          >
+            <br />
+            <Stack direction="row" spacing={1}>
+              <Chip icon={<VisibilityIcon />} label="20" color='primary' variant="outlined" />
+              <Chip icon={<StarIcon />} label="134" color='success' variant="outlined" />
+              <Chip icon={<InfoIcon />} label="1" color='error' variant="outlined" />
+              <Chip icon={<RestaurantIcon />} label="20" color='warning' variant="outlined" />
+            </Stack>
+          </Typography>
+        </React.Fragment>
+      }
+      />
+    </ListItem>
+    <Divider variant="inset" component="li" />
+  </>
   )
+}
+
+RepoItem.propTypes = {
+  repos: PropTypes.object.isRequired,
+
 }
 
 export default RepoItem
--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -50,7 +50,12 @@ export const GithubProvider = ({ children }) => {
   const getRepos = async (login) => {
 	  setLoading()
 
-    const response = await fetch(`${GITHUB_URL}/users/${login}/repos`);
+    const params = new URLSearchParams({
+      sort: 'created',
+      per_page: 10,
+    })
+
+    const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`);
     const data = await response.json();
 
     dispatch({
