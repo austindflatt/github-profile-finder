cc664a0599d76ced04b83be7638a1141a1879b20
Austin Flatt <80080997+austindflatt@users.noreply.github.com>
2022-03-09 01:03:01 -0500

    Now real data is showing for repos

--- src/components/Repos/RepoItem.jsx
+++ src/components/Repos/RepoItem.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 import ListItem from '@mui/material/ListItem'
 import ListItemText from '@mui/material/ListItemText'
 import Chip from '@mui/material/Chip'
@@ -26,7 +27,7 @@ const RepoItem = ({ repo }) => {
   } = repo;
   return (
 	<>
-  <ListItem alignItems="flex-start">
+  <ListItem alignItems="flex-start" href="https://www.google.com">
       <ListItemAvatar>
         <InsertLinkIcon />
       </ListItemAvatar>
@@ -40,12 +41,16 @@ const RepoItem = ({ repo }) => {
             variant="body2"
             color="text.primary"
           >
-            <br />
+            {description}
+            <br /><br />
             <Stack direction="row" spacing={1}>
-              <Chip icon={<VisibilityIcon />} label="20" color='primary' variant="outlined" />
-              <Chip icon={<StarIcon />} label="134" color='success' variant="outlined" />
-              <Chip icon={<InfoIcon />} label="1" color='error' variant="outlined" />
-              <Chip icon={<RestaurantIcon />} label="20" color='warning' variant="outlined" />
+              {language && (
+                <Chip label={language} variant="outlined" />
+              )}
+              <Chip icon={<VisibilityIcon />} label={watchers_count} color='primary' variant="outlined" />
+              <Chip icon={<StarIcon />} label={stargazers_count} color='success' variant="outlined" />
+              <Chip icon={<InfoIcon />} label={open_issues} color='error' variant="outlined" />
+              <Chip icon={<RestaurantIcon />} label={forks_count} color='warning' variant="outlined" />
             </Stack>
           </Typography>
         </React.Fragment>
