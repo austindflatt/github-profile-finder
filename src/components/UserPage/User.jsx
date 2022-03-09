08abf13aa2006dd539213e254958e83391cf59b3
Austin Flatt <80080997+austindflatt@users.noreply.github.com>
2022-03-09 01:20:00 -0500

    small fix

--- src/components/UserPage/User.jsx
+++ src/components/UserPage/User.jsx
@@ -59,7 +59,7 @@ const User = () => {
 	  <>
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={2} columns={16}>
-        <Grid item xs={12} md={6}>
+        <Grid item xs={12} md={6} lg={4}>
 
         <Avatar
           alt={login}
