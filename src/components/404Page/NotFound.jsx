5d1105db5deff34377e107c752dc4847e1658826
Austin Flatt <80080997+austindflatt@users.noreply.github.com>
2022-03-10 19:38:21 -0500

    fix: added to build script

--- src/components/404Page/NotFound.jsx
+++ src/components/404Page/NotFound.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function NotFound() {
   return (
-    <div>404</div>
+    <div style={{margin: 'auto', display: 'block'}}>
+      <h1>That page does not exist</h1>
+    </div>
   )
 }
 
